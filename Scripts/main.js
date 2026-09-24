13c29616503ad024029169f76a1d9c383b4f94d0
Aci <hoangn1626@gmail.com>
2026-09-24 16:28:00 +0700

    Update fetch path for body.html

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,7 @@
 import * as uhhh from "./uhhh_smth.js";
 
 async function loadContent() {
-    const response = await fetch("body.html");
+    const response = await fetch("../Updates/body.html");
     const content = await response.text();
     document.getElementById("content").innerHTML = content;
     uhhh.countdown();
