95e187a895c76734373b63cb7d777a7093dcc9f9
Aci <hoangn1626@gmail.com>
2026-09-24 16:08:43 +0700

    Add main.js to load HTML content asynchronously

---
+++ Scripts/main.js
@@ -0,0 +1,9 @@
+import * as uhhh from "./uhhh_smth.js";
+
+async function loadContent() {
+    const response = await fetch("body.html");
+    const content = await response.text();
+    document.getElementById("content").innerHTML = content;
+    uhhh.countdown();
+}
+loadContent();
