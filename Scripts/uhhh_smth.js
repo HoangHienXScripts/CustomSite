b0fb38e814677752a42ef30d109c00d99331ee63
Aci <hoangn1626@gmail.com>
2026-09-25 10:55:39 +0700

    Refactor element and countdown functions
    Refactor code for better readability by condensing if statements into single lines.

--- Scripts/uhhh_smth.js
+++ Scripts/uhhh_smth.js
@@ -1,8 +1,7 @@
+// tái cấu trúc nhỏ gọn nhất có thể :v
 function element(id) {
     let box = document.getElementById(id);
-    if (!box) {
-        box = "<p>Failed to get ElementId!</p>";
-    };
+    if (!box) { box = "<p>Failed to get ElementId!</p>"; }
     return box;
 };
 
@@ -15,10 +14,7 @@ function convert(str) {
 
 function countdown() {
     const html_cd_path = document.getElementById("countdown");
-    if (!html_cd_path) {
-        console.error("The #countdown element was not found.");
-        return;
-    }
+    if (!html_cd_path) { console.error("The #countdown element was not found."); return; }
     let time_left = 300;
     const update_cd = () => {
         const minutes = Math.floor(time_left / 60);
@@ -29,10 +25,7 @@ function countdown() {
     const timer = setInterval(() => { // setInterval(arg 1 : func, arg 2 : ms) return an object for clearInterval()
         time_left -= 1;
         update_cd();
-        if (time_left <= 0) {
-            clearInterval(timer); // clearInterval(arg 1 : object return from setInterval())
-            window.location.reload();
-        }
+        if (time_left <= 0) { clearInterval(timer); window.location.reload(); }
     }, 1000);
 }
 
