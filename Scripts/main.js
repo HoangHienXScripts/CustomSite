260f8e7cac043a05c07b0f0fd481eb4f4c458191
Aci <hoangn1626@gmail.com>
2026-09-25 15:08:02 +0700

    Simplify fetch logic in render_content function
    Refactor content rendering logic and simplify fetch calls.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,5 @@
 import {helper_func as hf} from "./uhhh_smth.js";
+
 async function render_content() {
     const box = document.getElementById("updated-content");
     if (!box) {
@@ -7,18 +8,18 @@ async function render_content() {
     }
     try {
         const rep = await fetch("Updates/body.html", {cache:"no-store"});
-        const cdx = await fetch("Updates/script_updates", {cache:"no-store"});
-        if (!rep.ok || !cdx.ok) {
+        // const cdx = await fetch("Updates/script_updates", {cache:"no-store"});
+        if (!rep.ok) {
             box.innerHTML = "<p>Failed to fetch smth...</p>";
         }
         const result_a = await rep.text();
-        let result_b = await cdx.text();
+        // let result_b = await cdx.text();
         box.innerHTML = result_a;
-        hf.cd();
-        hf.ph();
+        hf.cd(); hf.ph();
     } catch (err) {
         console.error(err);
         box.innerHTML = "<p>Unable to load updates right now.</p>";
     }
 }
+
 render_content();
