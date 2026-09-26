ea4eaca4fb11ea2d9abea35360ac5cf944bb1ae0
Aci <hoangn1626@gmail.com>
2026-09-26 11:41:02 +0700

    Refactor get_content to handle text and innerText
    Enhanced get_content function to support multiple methods for retrieving content.

--- Scripts/module.js
+++ Scripts/module.js
@@ -1,6 +1,16 @@
 // Shorten the funcs
 function id(i) { return document.getElementById(i); }
-function get_content(i) { return id(i).innerText; }
+function get_content(i, m) {
+  let x = m || 0;
+  if (x === 0) {
+    x = id(i).innerText;
+  } else if (x === 1) {
+    x = id(i).textContent;
+  } else {
+    x = `Invalid method: ${m}`;
+  }
+  return x;
+}
 function set_content(i, c) { id(i).textContent = c; }
 function load_html(i, c) { id(i).innerHTML = c; }
 
